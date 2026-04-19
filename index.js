8c6efbd2db7566997519a44d818161672a7d2a7e
paochirivi96-wq <paochirivi96@gmail.com>
2026-04-19 14:43:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,39 +4,35 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-const SHOPIFY_STORE = "TU-TIENDA.myshopify.com";
-const SHOPIFY_TOKEN = "TU_ACCESS_TOKEN";
+
+const SHOPIFY_STORE = "trento-8304.myshopify.com";
+const SHOPIFY_TOKEN = "TU_TOKEN_AQUI";
 
 app.post("/wompi/webhook", async (req, res) => {
   const event = req.body;
 
-  console.log("Evento recibido:", event);
+  console.log("Evento recibido:", JSON.stringify(event, null, 2));
 
   if (event.event === "transaction.updated") {
     const transaction = event.data.transaction;
 
     if (transaction.status === "APPROVED") {
-  const email = transaction.customer_email;
-  const amount = transaction.amount_in_cents / 100;
-
-  const order = await buscarPedido(email, amount);
-
-  if (!order) {
-    console.log("❌ No se encontró pedido");
-    return;
-  }
-
-  const orderId = order.id;
-
       try {
         await axios.post(
-          `https://${SHOPIFY_STORE}/admin/api/2024-01/orders/${orderId}/transactions.json`,
+          `https://${SHOPIFY_STORE}/admin/api/2024-01/orders.json`,
           {
-            transaction: {
-              kind: "capture",
-              status: "success",
-              amount: transaction.amount_in_cents / 100,
-              gateway: "Wompi"
+            order: {
+              email: transaction.customer_email,
+              financial_status: "paid",
+              send_receipt: true,
+              send_fulfillment_receipt: true,
+              line_items: [
+                {
+                  title: "Pedido desde Wompi",
+                  price: transaction.amount_in_cents / 100,
+                  quantity: 1
+                }
+              ]
             }
           },
           {
@@ -47,9 +43,9 @@ app.post("/wompi/webhook", async (req, res) => {
           }
         );
 
-        console.log("✅ Pedido pagado:", orderId);
+        console.log("✅ Pedido creado en Shopify");
       } catch (error) {
-        console.error("❌ Error:", error.response?.data || error.message);
+        console.error("❌ Error creando pedido:", error.response?.data || error.message);
       }
     }
   }
@@ -58,24 +54,3 @@ app.post("/wompi/webhook", async (req, res) => {
 });
 
 app.listen(3000, () => console.log("Servidor listo"));
-async function buscarPedido(email, amount) {
-  const SHOP = "TU-TIENDA.myshopify.com";
-  const TOKEN = "TU_ACCESS_TOKEN";
-
-  const response = await axios.get(
-    `https://${SHOP}/admin/api/2024-01/orders.json?status=open`,
-    {
-      headers: {
-        "X-Shopify-Access-Token": TOKEN
-      }
-    }
-  );
-
-  const orders = response.data.orders;
-
-  return orders.find(order =>
-    order.email === email &&
-    parseFloat(order.total_price) === amount &&
-    order.financial_status === "pending"
-  );
-}
